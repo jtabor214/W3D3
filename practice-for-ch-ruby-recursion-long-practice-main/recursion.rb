85dcc42f9107e9f6b9b6ae3ba684c9639bb795f6
“amaclarty” <andrewmaclarty@gmail.com>
2023-11-01 14:53:24 -0400

    exponentiation start

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -1,4 +1,5 @@
 def iter_range(first, last)
+
     numbers_between = []
     i = first
     while i < last 
@@ -8,4 +9,23 @@ def iter_range(first, last)
     numbers_between
 end
 
+def rec_range(first, last)
+
+    return [] if first == last 
+    return [] if last < first
+    [first] + rec_range(first + 1, last)
+
+end
+
+def exponentiation(base, power)
+    return 0 if base == 0
+    return base if power == 1
+    base * exponentiation(base, power - 1)
+end
+
+p exponentiation(3, 3)
+
+
+
 # p iter_range(2, 10)
+p rec_range(1, 4)
