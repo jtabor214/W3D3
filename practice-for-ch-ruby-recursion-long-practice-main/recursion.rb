4861c0c1453e60c91c987ec11fc14da393d7595d
Jacob Tabor <jtabor214@gmail.com>
2023-11-01 13:57:08 -0400

    iterative range

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -1,3 +1,12 @@
-def range(start, end)
-    return [start]
+def iter_range(first, last)
+    numbers_between = []
+    i = first
+    while i < last 
+        numbers_between << i 
+        i += 1
+    end
+    numbers_between
 end
+
+# p iter_range(2, 10)
+
